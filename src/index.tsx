888d67aea8135adb58a500288c72f096bda51e61
The-0mnipotent <arpit84_soe@jnu.ac.in>
2024-09-11 13:03:53 +0530

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const client = new ApolloClient({
-  uri: "https://tapme-arpit-backend.netlify.app/",
+  // uri: "https://tapme-arpit-backend.netlify.app/",
+  uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
